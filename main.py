ec3fa777049007775be84f64b4b96351195f5ab9
Banana_Soule <zachistoocool@gmail.com>
2019-12-10 14:41:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+n = input("How  Many Cups Of Water Have You Had Today? ")
+n = int(n)
+if n > 10 and n < 20:
+    print ("You May Be Hydrated, But You Should Drink Some More! The Average Adult Male Needs 15.5 Cups A Day! The Average Adult Women Needs At Least 11.5!")
+elif n < 10:
+    print ("You Should Drink More Water! The Average Adult Male Needs 15.5 Cups A Day! The Average Adult Women Needs At Least 11.5!")
+elif n == 10:
+    print ("You May Be Hydrated, But You Should Drink Some More! The Average Adult Male Needs 15.5 Cups A Day! The Average Adult Women Needs At Least 11.5!")
+elif n > 20:
+    print ("Be Careful! Water Is Great, But You Can Get Over-Hydrated!")
+else:
+  print ('Please Enter A Numeral!')
